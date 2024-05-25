1b7f8909f27d19f2717088c1f6dc1a53dd3afe7e
Guidy ange isaac <packer02andro69@gmail.com>
2024-05-25 22:47:42 +0100

    projet final

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ const express = require("express");
 const app = express();
 const port = process.env.port;
 const router = require("./routes/routes");
+const path = require("path");
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use((req, res, next) =>{
+    const date = new Date();
+    const day = date.getDay();
+    const hours = date.getHours();
+    
+    if((day < 1 || day > 5) && (hours < 9 || hours > 17)){
+        res.render("erreur", {title: "site indisponible"})
+    }else{
+        next();
+    }
+})
 app.use(express.json());
 app.use(router);
 
 
-app.listen(port, ()=>console.log(`server running at localhost:${port}`))
+app.listen(port, ()=>console.log(`server running at http://localhost:${port}`))
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.send("hello world");
+    res.render("index", {title: "Accueil"});
 });
-
+router.get("/contact", (req, res) =>{
+    res.render("../views/pages/contact", {title: "contact"});
+});
+router.get("/service", (req, res) =>{
+    res.render("../views/pages/services", {title: "Nos service"});
+})
 
 
 module.exports = router;
